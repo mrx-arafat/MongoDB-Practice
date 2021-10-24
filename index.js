b393521fd4ae35823f9af3f1470df5a0c6d07899
Easin Arafat <arafatmrx@gmail.com>
2021-10-24 07:49:26 +0600

    insert

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ const port = 4000;
 
 const uri =
   "mongodb+srv://mydbuser1:229NQc8yF9hwzGad@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 client.connect((err) => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("Nayika").collection("devices");
   // perform actions on the collection object
-  client.close();
+  console.log("hitting the db");
+
+  const user = {
+    name: "nurul hasan",
+    email: "yoyonurul@gmail.com",
+    phone: 01356654653,
+  };
+
+  collection.insertOne(user).then(() => {
+    console.log("insert success");
+  });
+
+  // client.close();
 });
 
 app.get("/", (req, res) => {

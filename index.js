223f9d08165690de327b5bc6d58a07355b266e6e
Easin Arafat <arafatmrx@gmail.com>
2021-10-24 23:18:43 +0600

    gunius car mecha

--- index.js
+++ index.js
@@ -1,61 +1,13 @@
-const { MongoClient } = require("mongodb");
 const express = require("express");
 
 const app = express();
 
 const port = 4000;
 
-//user: mydbuser1
-//password: 229NQc8yF9hwzGad
-
-const uri =
-  "mongodb+srv://mydbuser1:229NQc8yF9hwzGad@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db("insertDB");
-    const haiku = database.collection("haiku");
-    // create a document to insert
-    const doc = {
-      name: "Easin Arafat",
-      email: "e4rafat@gmail.com",
-    };
-    const result = await haiku.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-// client.connect((err) => {
-//   const collection = client.db("Nayika").collection("devices");
-//   // perform actions on the collection object
-//   console.log("hitting the db");
-
-//   const user = {
-//     name: "nurul hasan",
-//     email: "yoyonurul@gmail.com",
-//     phone: 01356654653,
-//   };
-
-//   collection.insertOne(user).then(() => {
-//     console.log("insert success");
-//   });
-
-//   // client.close();
-// });
-
 app.get("/users", (req, res) => {
-  res.send("yo yo honey");
+  res.send("dhur node er mai re chudi. valoi pera diteche still ");
 });
 
 app.listen(port, () => {
-  console.log("running server on port", port);
+  console.log("node ajke maire baap kore dibo");
 });

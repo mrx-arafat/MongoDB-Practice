486e0fb1ad3f69d5141140952c69d9a92edb26e9
Easin Arafat <arafatmrx@gmail.com>
2021-10-24 06:44:54 +0600

    mongo update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
 
 const app = express();
@@ -6,6 +7,18 @@ const port = 4000;
 
 //user: mydbuser1
 //password: 229NQc8yF9hwzGad
+
+const uri =
+  "mongodb+srv://mydbuser1:229NQc8yF9hwzGad@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("yo yo honey");
